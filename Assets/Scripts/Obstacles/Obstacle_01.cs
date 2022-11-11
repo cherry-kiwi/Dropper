93fc25b36227210bb23de52659ada834e48e7283
cherry-kiwi <86244674+cherry-kiwi@users.noreply.github.com>
2022-11-12 01:10:27 +0900

    debug

--- Assets/Scripts/Obstacles/Obstacle_01.cs
+++ Assets/Scripts/Obstacles/Obstacle_01.cs
@@ -11,7 +11,7 @@ public class Obstacle_01 : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody>();
 
-        PlayerCtrl playerCtrl = GameObject.Find("HP").GetComponent<PlayerCtrl>(); //Player�� HP �ҷ���
+        playerCtrl = GameObject.Find("HP").GetComponent<PlayerCtrl>(); //Player�� HP �ҷ���
     }
 
     void Update()
