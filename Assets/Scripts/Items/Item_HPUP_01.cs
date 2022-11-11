45cea2f536e99b25db44689ce67477e0a82d405d
cherry-kiwi <86244674+cherry-kiwi@users.noreply.github.com>
2022-11-11 22:51:59 +0900

    Item Scripts Edited

---
+++ Assets/Scripts/Items/Item_HPUP_01.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_HPUP_01 : MonoBehaviour
+{
+    PlayerCtrl playerCtrl;
+    public Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+
+        PlayerCtrl playerCtrl = GameObject.Find("HP").GetComponent<PlayerCtrl>(); //Player의 HP 불러옴
+        playerCtrl.HP = 100.0f;
+    }
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            if (playerCtrl.HP < 100) //HP가 100미만일때
+            {
+                playerCtrl.HP += 20; //HP에 20 힐하기
+
+                if (playerCtrl.HP >= 100) //HP가 100이상 될때
+                {
+                    playerCtrl.HP = 100; //100으로 맞춰주기
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Items/Item_ScoreUP.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_ScoreUP : MonoBehaviour
+{
+    PlayerCtrl playerCtrl;
+    public Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+
+        PlayerCtrl playerCtrl = GameObject.Find("score").GetComponent<PlayerCtrl>(); //PlayerÀÇ HP ºÒ·¯¿È
+        playerCtrl.score = 0.0f;
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            playerCtrl.score += 100.0f;
+        }
+    }
+}
