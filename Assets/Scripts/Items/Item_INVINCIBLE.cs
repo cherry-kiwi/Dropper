0efa97f1dec1b69793527ab638a0e0c43f8a4e7a
cherry-kiwi <86244674+cherry-kiwi@users.noreply.github.com>
2022-11-13 04:14:02 +0900

    Scripts Edited

--- Assets/Scripts/Items/Item_INVINCIBLE.cs
+++ Assets/Scripts/Items/Item_INVINCIBLE.cs
@@ -4,45 +4,67 @@ using UnityEngine;
 
 public class Item_INVINCIBLE : MonoBehaviour
 {
-    private float invincibleTime; //무적 상태의 시간이 얼마나 흘렀는가 판단하기 위한 변수
-    bool isInvincible; //무적 상태인가 판단
-    public GameObject PostProcessingCamera;
+    Main_Camera Cam;
+    Director director;
+    Rigidbody rigid;
 
     void Start()
     {
-        isInvincible = false; //무적 상태 해제
-        PostProcessingCamera.gameObject.SetActive(false); //포스트 프로세싱 관리하는 카메라 비활성화
-    }
-
-    void Update()
-    {
-        
-    }
-
-    void Invincible()
-    {
-        if (isInvincible) //무적 상태일 때
-        {
-            PostProcessingCamera.gameObject.SetActive(true); //포스트 프로세싱 관리하는 카메라 활성화
-        }
+        rigid = GetComponent<Rigidbody>();
+        director = GameObject.Find("Director").GetComponent<Director>();
+        Cam = GameObject.Find("Main Camera").GetComponent<Main_Camera>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Player")
         {
-            if(invincibleTime > 0) //무적 시간이 흐르지 않았을 때
-            {
-                isInvincible = true; //무적 상태 설정
-
-                Invincible();
-                invincibleTime += 1 * Time.deltaTime; //무적 시간 1초씩 흐르게
-            }
-
-            else if (invincibleTime <= 3)
-            {
-
-            }
+            Cam.isInvincible = true;
+            Destroy(gameObject);
         }
     }
+
+    //private float invincibleTime; //무적 상태의 시간이 얼마나 흘렀는가 판단하기 위한 변수
+    //bool isInvincible; //무적 상태인가 판단
+    //public GameObject PostProcessingCamera;
+
+    //void Start()
+    //{
+    //    Cam = GameObject.Find("Main Camera").GetComponent<Main_Camera>();
+
+    //    isInvincible = false; //무적 상태 해제
+    //    PostProcessingCamera.gameObject.SetActive(false); //포스트 프로세싱 관리하는 카메라 비활성화
+    //}
+
+    //void Update()
+    //{
+
+    //}
+
+    //void Invincible()
+    //{
+    //    if (isInvincible) //무적 상태일 때
+    //    {
+    //        PostProcessingCamera.gameObject.SetActive(true); //포스트 프로세싱 관리하는 카메라 활성화
+    //    }
+    //}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.name == "Player")
+    //    {
+    //        if(invincibleTime > 0) //무적 시간이 흐르지 않았을 때
+    //        {
+    //            isInvincible = true; //무적 상태 설정
+
+    //            Invincible();
+    //            invincibleTime += 1 * Time.deltaTime; //무적 시간 1초씩 흐르게
+    //        }
+
+    //        else if (invincibleTime <= 3)
+    //        {
+
+    //        }
+    //    }
+    //}
 }
