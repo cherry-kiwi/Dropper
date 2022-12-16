be3e00e666762727a4c5fa3a5564a081eeae8272
cherry-kiwi <hmongsangga@gmail.com>
2022-12-16 09:13:57 +0900

    obs walls

---
+++ Assets/Scripts/Obstacles/Obstacle_2Wall_2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_2Wall_2 : MonoBehaviour
+{
+    int a = 1;
+
+    void Update()
+    {
+        if (transform.position.z >= -2.5f)
+        {
+            a = -1;
+        }
+        else if (transform.position.z < -4.5f)
+        {
+            a = 1;
+        }
+
+        transform.Translate(Vector3.up * 1.0f * Time.deltaTime * a);
+    }
+}
